799aebbea6a18d903672e388ddb6e8b673b9d826
Amit Bobade <amit.bobade@synerzip.com>
2018-11-28 12:02:43 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, Alert} from 'react-native';
+import codePush from "react-native-code-push";
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>This hosting app changes demo.</Text>
+        <Button onPress={this.onPressLearnMore} title="Learn More" color="#841584"
+        accessibilityLabel="Learn more about this purple button"/>
+      </View>
+    );
+  }
+
+  onPressLearnMore() {
+    Alert.alert('Test', 'Learn more button pressed.');
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+App = codePush(codePushOptions)(App);
